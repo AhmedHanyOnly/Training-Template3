5f552393d056656af2ffa59b8a0d6d14526aa3b1
ahmed <mommentcraft@gmail.com>
2023-06-22 21:10:32 +0300

    template training

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let lis = document.querySelectorAll(".practices ul li");
+
+lis.forEach((li) => {
+  li.addEventListener("click", (e) => {
+    lis.forEach((li) => {
+      li.classList.remove("active");
+    });
+    e.currentTarget.classList.add("active");
+  });
+});
